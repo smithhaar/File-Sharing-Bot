b40678a1d8f9a215c2c1281f98ccadfe2453b6a7
Garijon3 <105334993+Garijon3@users.noreply.github.com>
2023-02-26 10:41:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<code>{file_name}</code> \n\n<b>ᴊᴏɪɴ ɴᴏᴡ</b>: [Namo](https://t.me/+lWqyUqodj5thMzNh)")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "\n\n<b>ᴊᴏɪɴ ɴᴏᴡ</b>: [Namo](https://t.me/+lWqyUqodj5thMzNh)")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
@@ -64,7 +64,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = ""
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(880087645)
+ADMINS.append(1823620269)
 
 LOG_FILE_NAME = "filesharingbot1.txt"
 
