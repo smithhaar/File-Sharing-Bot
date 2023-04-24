4980bf677e6894faa13af6e8256ff90464dda97a
smithhaar <124481464+smithhaar@users.noreply.github.com>
2023-04-24 16:22:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://random:random@cluster0.tb
 DB_NAME = os.environ.get("DATABASE_NAME", "filestore5")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001742838735 -1001736636305"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001736636305"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
